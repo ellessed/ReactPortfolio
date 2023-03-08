7d3d108b8229941cb8a5f68c14031d21159a6527
elleserdonaldson <ellessedon@gmail.com>
2023-03-08 21:55:44 +0000

    basic footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React from "react";
+import Footer from "./components/Footer";
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+const App = () => {
+  return (
+    <div>
+      <PortfolioContainer />
+      {/* add footer component here */}
+      <Footer />
+    </div>
+    // add header component
+  );
+};
 
 export default App;
