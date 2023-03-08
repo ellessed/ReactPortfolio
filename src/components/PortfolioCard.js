fa1699e289704b4151a2a35797bbc192562f439e
elleserdonaldson <ellessedon@gmail.com>
2023-03-08 21:55:59 +0000

    rendered data to card

--- src/components/PortfolioCard.js
+++ src/components/PortfolioCard.js
@@ -1,18 +1,19 @@
 const PortfolioCard = (props) => {
-  const { project } = props;
+  const { projectid, name, image, gitHub, deployLink, description } =
+    props.project;
+  console.log(props);
   return (
-    <div>
-      <div>
-        <div className="card" style={{ width: "18rem" }}>
-          <img src="..." className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{project.portfolioTitle}</h5>
-            <p className="card-text">{project.description}</p>
-            <a href={project.href} className="btn btn-primary">
-              Go somewhere
-            </a>
-          </div>
-        </div>
+    <div className="card m-5 " style={{ width: "25rem" }}>
+      <img src={image} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{name}</h5>
+        <p className="card-text">{description}</p>
+        <a href={gitHub} className="btn btn-primary mx-1">
+          GitHub Repo
+        </a>
+        <a href={deployLink} className="btn btn-primary mx-1">
+          Deployed App
+        </a>
       </div>
     </div>
   );
