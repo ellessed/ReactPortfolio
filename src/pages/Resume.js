f3029fdd50c17cab67f6c5e85b5be60cbb4060ad
elleserdonaldson <ellessedon@gmail.com>
2023-03-08 22:34:55 +0000

    created resume container and link to cv

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 export default function Resume() {
   return (
     <div>
       <h1>Resume </h1>
-      <p>CV here</p>
+      <p>Hey check out my Resume below</p>
+      <div className="resume">
+        <Button
+          variant="Link"
+          href="https://docs.google.com/document/d/e/2PACX-1vQLIZ5rr4XpJjVpeeGKyXEqSIP-9SPVuJy-nXvWKGV6hQWO2Rp5fDvzKZo8ItJnfQ/pub"
+          target="_blank"
+          rel="noreferrer"
+          className="card-button"
+        >
+          View Resume
+        </Button>
+      </div>
     </div>
   );
 }
