06643d41ac27328fcc20c45b3238b110a0960e52
elleserdonaldson <ellessedon@gmail.com>
2023-03-09 18:03:32 +0000

    entered p tag with about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,17 @@
 import React from "react";
+import myself from "../assets/My project.png";
 
 export default function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>Hi my name is Ellesse</p>
-    </div>
+    <>
+      <div className="myself-img">
+        <img alt="developer" src={myself} />
+        <p>
+          Hello. My name is Ellesse. I am a Juniour Web Developer from
+          Birmingham. I enjoy learning how things work, fixing things that are
+          broken and bringing code to life!
+        </p>
+      </div>
+    </>
   );
 }
