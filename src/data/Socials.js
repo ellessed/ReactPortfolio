c1eefb7aaa36b4ac183ebde20db03544fc7a54c2
elleserdonaldson <ellessedon@gmail.com>
2023-03-10 19:42:21 +0000

    data created for socialcontent

---
+++ src/data/Socials.js
@@ -0,0 +1,22 @@
+export const ConnectData = [
+  {
+    platform: "Github",
+    icon: require("../assets/icons/github.png"),
+    link: "https://github.com/ellessed?tab=repositories",
+  },
+  {
+    platform: "Youtube",
+    icon: require("../assets/icons/youtube.png"),
+    link: "https://www.youtube.com/@ellesse4459/about",
+  },
+  {
+    platform: "Twitter",
+    icon: require("../assets/icons/twitter.png"),
+    link: "https://twitter.com/",
+  },
+  {
+    platform: "Facebook",
+    icon: require("../assets/icons/facebook.png"),
+    link: "https://www.facebook.com/",
+  },
+];
