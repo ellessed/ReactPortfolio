75a05c43bd5663455ec5066ad141ab4866db1358
elleserdonaldson <ellessedon@gmail.com>
2023-03-12 20:36:23 +0000

    restructured aboutme page

---
+++ src/components/separator/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../index.css";
+
+const Separator = () => {
+  return <div className="separator"></div>;
+};
+
+export default Separator;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,25 +1,44 @@
 import React from "react";
-import "../index.css";
-import Social from "../components/SocialContent";
+import myself from "../assets/AvatarEllesse.png";
 
-function AboutMe() {
+const AboutMe = () => {
   return (
-    <div className="about">
-      <div className="about-top">
-        <div className="about-info">
-          Ellesse
-          <br /> WEB DEVELOPER
-        </div>
-        <div className="about-photo">
-          <img
-            src={require("../assets/AvatarEllesse.png")}
-            className="picture"
-          />
+    <>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3">
+          <h1>About Me</h1>
+          <div className="about">
+            <div className="bio">
+              <p>
+                Welcome to my portfolio page! My name is Ellesse and I am a
+                junior software developer with a passion for creating innovative
+                and impactful solutions through code. I am excited to showcase
+                my skills, experience, and projects to demonstrate my
+                proficiency in various programming languages, tools, and
+                technologies. As a junior developer, I am constantly seeking to
+                learn and improve my craft, and this portfolio is a reflection
+                of my dedication to honing my skills. My goal is to contribute
+                to the development of meaningful applications that make a
+                positive difference in people's lives. In this portfolio, you
+                will find examples of my work, ranging from personal projects to
+                team collaborations. Each project demonstrates my
+                problem-solving abilities, creativity, and attention to detail.
+                I am confident that my portfolio will illustrate my potential as
+                a valuable addition to any software development team. Thank you
+                for visiting my page, and I hope you enjoy exploring my work!
+              </p>
+            </div>
+            <div style={{ width: "30%" }}>
+              <img
+                className="myself-image"
+                alt="this is a picture of myself the developer"
+                src={myself}
+              />
+            </div>
+          </div>
         </div>
       </div>
-      <Social />
-    </div>
+    </>
   );
-}
-
+};
 export default AboutMe;
